ea32c8fedaa645edc24f84bdc0196ae7a92b86a9
tayl3 <120345291+tayl3@users.noreply.github.com>
2023-03-04 17:55:26 -0600

    Update About.js

--- src/About.js
+++ src/About.js
@@ -3,6 +3,7 @@ import React from "react";
 //Write an About component that renders a short paragraph about the site.
 const About = () => {
     return (
+<<<<<<< Updated upstream
         <div class = "body d-flex">
             <div class = "my-auto">
                 <h1 style={{color: "lightblue"}}>Welcome Everyone!</h1>
@@ -21,6 +22,18 @@ const About = () => {
                     their health and for their college experience!
                 </p>
             </div>
+=======
+        <div class = "body align-items-center">      
+        <h1 style={{color: "lightblue"}}>Welcome everyone!</h1>
+        <p style={{border: "3px solid lightblue"}}>
+            Welcome to TransparentUni! We are a resource for prospective college students to connect with current college students
+            and to get questions answered about mental health and disability accommodations and accessibility on campus. We hope to establish
+            a community of current college students with accommodations who are willing to share their experiences and be mentors for prospective
+            students who are looking for similar accommodations when they attend college. Our mission to provide a safe space for students to get the
+            information they need to make the best decision for their health and for their college experience!
+        </p>
+        <img className="aboutImage" src="./assets/brains.png" alt="brains"/>
+>>>>>>> Stashed changes
         </div>
     );
     }
