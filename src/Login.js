ebbb25dd95fac71a6977f2e2904317f02119770c
Param Oza <paramoza@Params-MacBook-Pro.local>
2023-03-04 14:01:14 -0600

    google auth img

--- src/Login.js
+++ src/Login.js
@@ -5,9 +5,8 @@ import './App.css';
 const Login = () => {
     return (
         <div className="login-page">
-                  <button className="button" onClick={signInWithGoogle}><i className="fab fa-google"></i>Sign in with google</button>
+                  <a href='#' onClick={signInWithGoogle}><img src='./assets/sign_in.png'/></a>
         </div>
     )
 }
-
 export default Login;
