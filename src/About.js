8774f65c38ad6a8cf3e3805399c4c9cee269c93e
evanedwardsgit <evanedwards1024@protonmail.com>
2023-03-04 17:09:17 -0600

    sfaed

--- src/About.js
+++ src/About.js
@@ -3,15 +3,15 @@ import React from "react";
 //Write an About component that renders a short paragraph about the site.
 const About = () => {
     return (
-        <div class = "body align-items-center">      
-        <h1 style={{color: "lightblue"}}>About</h1>
-        <p style={{border: "3px solid lightblue"}}>
-            Welcome to TransparentUni! We are a resource for prospective college students to connect with current college students
-            and get questions answered about mental health and disability accommodations and accessibility on campus. We hope to establish
-            a community of current college students with accommodations who are willing to share their experiences and be mentors for prospective
-            students who are looking for similar accommodations when they attend college. Our mission to provide a safe space for students to get the
-            information they need to make the best decision for their health and for their college experience!
-        </p>
+        <div class = " ">      
+            <h1 style={{color: "lightblue"}}>About</h1>
+            <p style={{border: "3px solid lightblue"}}>
+                Welcome to TransparentUni! We are a resource for prospective college students to connect with current college students
+                and get questions answered about mental health and disability accommodations and accessibility on campus. We hope to establish
+                a community of current college students with accommodations who are willing to share their experiences and be mentors for prospective
+                students who are looking for similar accommodations when they attend college. Our mission to provide a safe space for students to get the
+                information they need to make the best decision for their health and for their college experience!
+            </p>
         </div>
     );
     }
