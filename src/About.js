b604bd0f4d01b34f422e8bab666a8fa2939a4c39
tayl3 <120345291+tayl3@users.noreply.github.com>
2023-03-04 17:53:52 -0600

    Split up text

--- src/About.js
+++ src/About.js
@@ -5,15 +5,21 @@ const About = () => {
     return (
         <div class = "body d-flex">
             <div class = "my-auto">
-                <h1>About</h1>
+                <h1 style={{color: "lightblue"}}>Welcome Everyone!</h1>
                 <p>
                     Welcome to TransparentUni! We are a resource for prospective college students to connect with current college students
                     and get questions answered about mental health and disability accommodations and accessibility on campus. We hope to establish
                     a community of current college students with accommodations who are willing to share their experiences and be mentors for prospective
-                    students who are looking for similar accommodations when they attend college. Our mission to provide a safe space for students to get the
-                    information they need to make the best decision for their health and for their college experience!
+                    students who are looking for similar accommodations when they attend college.
                 </p>
             <img className="aboutImage" src="./assets/brains.png" alt="brains"/>
+            <br></br>
+            <h2 style={{color: "lightblue"}}>Our Mission</h2>
+                <p style>
+                    Our mission to end the stigma around mental health and disability accommodations and to provide a safe space
+                    for all prospective college students to get the necessary information they need to make the best decision for
+                    their health and for their college experience!
+                </p>
             </div>
         </div>
     );
