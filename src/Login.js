98a69eea6618363b5720024d53b1a2b7432a6b6a
evanedwardsgit <evanedwards1024@protonmail.com>
2023-03-04 14:14:19 -0600

    Change height/width of login

--- src/Login.js
+++ src/Login.js
@@ -5,7 +5,7 @@ import './App.css';
 const Login = () => {
     return (
         <div className="login-page">
-                  <a href='#' onClick={signInWithGoogle}><img src='./assets/sign_in.png'/></a>
+                  <a href='#' onClick={signInWithGoogle}><img width = "191" height = "46" src='./assets/sign_in.png'/></a>
         </div>
     )
 }
