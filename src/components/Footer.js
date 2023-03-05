6c95778548b44cc61c679b9646805af00f2064c7
evanedwardsgit <evanedwards1024@protonmail.com>
2023-03-04 22:20:00 -0600

    fooooooooooooooter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,13 @@ import '../App.css';
 const Footer = () => {
     return (
         <Container>
-          <hr class="hr" />
-          <h2>&copy; 2023</h2>
-          <h3>Param Oza, Taylor Edwards, Evan Edwards</h3>
+          <div class = "d-flex justify-content-center">
+            <div>
+                <hr class="hr" />
+              <h2>&copy; 2023</h2>
+              <h3>Param Oza, Taylor Edwards, Evan Edwards</h3>`
+            </div>
+            </div>
         </Container>
     )
 }
