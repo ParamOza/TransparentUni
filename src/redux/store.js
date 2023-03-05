d0f71d46a339890b80e1e0539369bd63292101fa
tayl3 <120345291+tayl3@users.noreply.github.com>
2023-03-04 20:43:02 -0600

    Fixed sign up and log in buttons

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import modalReducer from './modalSlice';
+import loginModalReducer from './loginModalSlice';
 
 export default configureStore({
     reducer: {
         modal: modalReducer,
+        loginModal: loginModalReducer,
     },
 })
