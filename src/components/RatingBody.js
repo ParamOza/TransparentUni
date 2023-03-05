059f51d98f5e2f36920a12555a2e0ba33d0e8e6c
evanedwardsgit <evanedwards1024@protonmail.com>
2023-03-05 01:18:36 -0600

    Rating Start

---
+++ src/components/RatingBody.js
@@ -0,0 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+const RatingBody = () => {
+    return (
+      <Container>
+            <div class =  "d-flex">
+            <div class = "my-auto">
+                <p>
+                    Welcome to TransparentUni rankings. Here you will find the highest alumni and student ranked universities based on their accessibility and accommodations for disabilities.
+                </p>
+            </div>
+        </div>
+      </Container>
+    )
+}
+
+export default RatingBody;
